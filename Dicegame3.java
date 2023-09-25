4c9cab6d3a946edf6e7001a99165487c0acf9525
Mo Zaabat <mzaabat@hotmail.com>
2023-09-25 15:32:56 +0200

    sidstespin

--- Dicegame3.java
+++ Dicegame3.java
@@ -28,6 +28,8 @@ public class Dicegame3 {
                 int dice2 = rollDice(random);
                 int sum1 = dice1 + dice2;
                 System.out.println(player1Name + " rullede: " + dice1 + " og " + dice2);
+                
+                
 
                 if (dice1 == 1 && dice2 == 1) {
                     System.out.println("ups du rullede to 1'ere");
@@ -39,18 +41,15 @@ public class Dicegame3 {
                 System.out.println(player1Name + "'s point: " + (player1Score));
             
 
-<<<<<<< HEAD
             //Hvis player1 rammer 2 6'ere
 
-            if (dice1==6 && dice2==6){
+            if ((dice1-1 ==6 && dice2-1 ==6) && (dice1 == 6 && dice2 == 6)){
                 System.out.println(player1Name + "har slået to 6'ere! " +player1Name + " vinder!!!!");
                 break;
             }
 
             System.out.println(player1Name + " rullede: " + dice1 + " og " + dice2);
             System.out.println(player1Name + "'s point: " + (player1Score += sum1));
-=======
->>>>>>> Development
 
             if (player1Score >= 40) {
                 System.out.println(player1Name +  "vinder!!!!");
@@ -66,13 +65,14 @@ public class Dicegame3 {
 
             
             //Hvis player2 slår to 6'ere
-            if (dice3 == 6 && dice4 == 6){
+            if ((dice3-1 == 6 && dice4-1 == 6) && (dice3 == 6 && dice4 == 6)){
                 System.out.println(player2Name + "har slået to 6'ere! " +player2Name + " vinder!!!!");
                 break;
 
             }
 
             System.out.println(player2Name + " rullede: " + dice3 + " og " + dice4);
+        
 
             if (dice3 == 1 && dice4 == 1) {
                 System.out.println("ups du rullede to 1'ere");
@@ -87,27 +87,14 @@ public class Dicegame3 {
             if (player2Score >= 40) {
                 System.out.println(player2Name + " vinder!!!!");
                 break;
-            }
-<<<<<<< HEAD
-
-        }
-=======
-            turn = 0;
-
-        }
-    }
->>>>>>> Development
+            } 
+            turn = 1;
+        }}
         scanner.close();
-
-
-
-
-    }
+    
+}
     private static int rollDice(Random random) {
         return random.nextInt(6) + 1;
     }
-    
-    
-
     
 }
